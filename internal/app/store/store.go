be0cb36c07f2d97fac0c204932cbd0546dce6508
Carlos Venkman <cvenkman@si-f3.kzn.21-school.ru>
2022-05-07 00:03:15 +0300

    ?

--- internal/app/store/store.go
+++ internal/app/store/store.go
@@ -1,7 +1,14 @@
 package store
 
+import (
+	"database/sql"
+	
+	_ "github.com/lib/pq"
+)
+
 type Store struct {
 	config *Config
+	db *sql.DB
 }
 
 func (store *Store) New(config *Config) *Store {
@@ -11,9 +18,18 @@ func (store *Store) New(config *Config) *Store {
 }
 
 func (store *Store) Open() error {
+	db, err := sql.Open("postgres", store.config.DatabaseURL)
+	if err != nil {
+		return err
+	}
+	if err := db.Ping(); err != nil {
+		return err
+	}
+
+	store.db = db
 	return nil
 }
 
 func (store *Store) Close() {
-
+	store.db.Close()
 }
